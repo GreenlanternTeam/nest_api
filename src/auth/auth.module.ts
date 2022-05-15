c7871b1b6f14f01192345a7b1e2c9f8934c3b386
raymondanythings <akdfhr2@gmail.com>
2022-05-15 15:52:42 +0900

    heroku deploy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,22 +1,22 @@
-import { jwtConstants } from './constants';
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from 'src/users/users.module';
-import { AuthService } from './auth.service';
-import { LocalStrategy } from './local.strategy';
-import { JwtStrategy } from './jwt.strategy';
+import { jwtConstants } from './constants'
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { UsersModule } from 'src/users/users.module'
+import { AuthService } from './auth.service'
+import { LocalStrategy } from './local.strategy'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || jwtConstants.secret,
-      signOptions: { expiresIn: '1m' },
-    }),
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService],
+	imports: [
+		UsersModule,
+		PassportModule,
+		JwtModule.register({
+			secret: process.env.JWT_SECRET || jwtConstants.secret,
+			signOptions: { expiresIn: '1m' }
+		})
+	],
+	providers: [AuthService, LocalStrategy, JwtStrategy],
+	exports: [AuthService]
 })
 export class AuthModule {}
